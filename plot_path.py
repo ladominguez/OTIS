6b2d3211bea6b1b375fb952636cfae3c3e2087a1
Luis Antonio Dominguez <ladominguez@ucla.edu>
2024-02-10 22:28:02 -0600

    reading locations of ssn stations

--- plot_path.py
+++ plot_path.py
@@ -10,6 +10,9 @@ def plot_data(data, title, xlabel, ylabel):
     plt.show()
     return  
 
+def read_stations_ssn(filename='./otis/stations_ssn.dat'):
+    stations = pd.read_csv(filename, delim_whitespace=True,names=['station','latitude','longitude'],index_col=0)
+    return stations
 
 def read_path(filename):
     path = pd.read_csv(filename, delim_whitespace=True, 
@@ -28,8 +31,13 @@ def distance_to_station(latitude, longitude, path):
 
 
 if __name__ == "__main__":
+    station = 'CAIG'
+    stations = read_stations_ssn()
+    latitude = stations.loc[station, 'latitude']
+    longitude = stations.loc[station, 'longitude']
+
     time, path = read_path('hurracaine_path.dat')
 
-    distances = distance_to_station(25.7617, -80.1918, path)
+    distances = distance_to_station(latitude, longitude, path)
     print(distances)
     pass
